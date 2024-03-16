88bb1e83e207135497f1c5a77416943011659320
AlaranUmar <alaranumar@gmail.com>
2024-03-16 17:06:00 +0100

    calc and login

---
+++ calc.js
@@ -0,0 +1,20 @@
+// Calculator Program
+
+const display = document.getElementById("display");
+
+function appendToDisplay(input) {
+  // check chat gpt
+  display.value += input;
+}
+
+function clearDisplay() {
+  display.value = "";
+}
+
+function calculate() {
+  try {
+    display.value = eval(display.value);
+  } catch (error) {
+    display.value = "Error";
+  }
+}
